6c9d5c59ac6d1ed5212302ba90de704637da6509
Adela2012 <15757124551@163.com>
2017-05-27 14:36:08 +0800

    change msite --swipe

--- src/pages/msite/msite.vue
+++ src/pages/msite/msite.vue
@@ -12,29 +12,59 @@
       </router-link>
     </head-top>
     <nav class="msite_nav">
-      <div class="swiper-container">
+      <swipe  :auto="0" class="foodentry">
+        <swipe-item v-for="(item, index) in foodTypes" :key="index"  class="food_types_container">
+          <router-link :to="{path: '/food', query: {geohash, title: foodItem.title, restaurant_category_id: getCategoryId(foodItem.link)}}" v-for="foodItem in item" :key="foodItem.id" v-if="foodItem.title !== '预定早餐'" class="link_to_food">
+            <figure>
+              <img :src="imgBaseUrl + foodItem.image_url" alt="">
+              <figcaption>{{foodItem.title}}</figcaption>
+            </figure>
+          </router-link>
+          <a href="https://zaocan.ele.me/" class="link_to_food" v-else>
+            <figure>
+              <img :src="imgBaseUrl + foodItem.image_url" alt="">
+              <figcaption>{{foodItem.title}}</figcaption>
+            </figure>
+          </a>
+        </swipe-item>
+      </swipe>
+<!--       <div class="swiper-container">
         <div class="swiper-wrapper">
-          <div class="swiper-slide food_types_container"></div>
+          <div class="swiper-slide food_types_container" v-for="(item, index) in foodTypes" :key="index">
+              <router-link :to="{path: '/food', query: {geohash, title: foodItem.title, restaurant_category_id: getCategoryId(foodItem.link)}}" v-for="foodItem in item" :key="foodItem.id" v-if="foodItem.title !== '预定早餐'" class="link_to_food">
+                <figure>
+                  <img :src="imgBaseUrl + foodItem.image_url" alt="">
+                  <figcaption>{{foodItem.title}}</figcaption>
+                </figure>
+              </router-link>
+              <a href="https://zaocan.ele.me/" class="link_to_food" v-else>
+                <figure>
+                  <img :src="imgBaseUrl + foodItem.image_url" alt="">
+                  <figcaption>{{foodItem.title}}</figcaption>
+                </figure>
+              </a>
+          </div>
         </div>
         <div class="swiper-pagination"></div>
-      </div>
+      </div> -->
     </nav>
   </div>
 </template>
 
 <script>
 import headTop from '@/components/header/head'
+import {swipe, swipeItem} from '@/components/mint/swipe'
 import {msiteAddress, msiteFoodTypes} from '@/service/getData'
 import {mapMutations} from 'vuex'
-// import '@/plugins/swiper.min.js'
-// import '@/style/swiper.min.css'
+import {imgBaseUrl} from '@/config/env'
 export default {
   data () {
     return {
       msiteTitle: '请选择地址...', // msiet页面头部标题
       foodTypes: [], // 食品分类列表
       geohash: '',
-      hasGetData: false // 是否已经获取地理位置数据，成功后获取商铺列表信息
+      hasGetData: false, // 是否已经获取地理位置数据，成功后获取商铺列表信息
+      imgBaseUrl
     }
   },
   async beforeMount () {
@@ -50,25 +80,30 @@ export default {
   },
   mounted () {
     msiteFoodTypes(this.geohash).then(res => {
-      let resLength = res.resLength
+      let resLength = res.length
       let resArr = res.concat([]) // 返回一个新的数组
       let foodArr = []
       for (let i = 0, j = 0; i < resLength; i += 8, j++) {
         foodArr[j] = resArr.splice(0, 8)
       }
       this.foodTypes = foodArr
-    }).then(() => {
-      // new Swiper('.swiper-container', {
-      //   pagination: '.swiper-pagination',
-      //   loop: true
-      // })
     })
   },
   methods: {
-    ...mapMutations(['SAVE_GEOHASH', 'RECORD_ADDRESS'])
+    ...mapMutations(['SAVE_GEOHASH', 'RECORD_ADDRESS']),
+    getCategoryId (url) {
+      let urlData = decodeURIComponent(url.split('=')[1].replace('&target_name', ''))
+      if (/restaurant_category_id/gi.test(urlData)) {
+        return JSON.parse(urlData).restaurant_category_id.id
+      } else {
+        return ''
+      }
+    }
   },
   components: {
-    headTop
+    headTop,
+    swipe,
+    swipeItem
   }
 }
 </script>
@@ -76,6 +111,12 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
   @import 'src/style/mixin';
+  .foodentry {
+    overflow: hidden;
+    height: 8rem;
+    background-color: #fff;
+    text-align: center;
+  }
   .link_search{
     left: .8rem;
     @include wh(.8rem, .9rem);
@@ -106,15 +147,13 @@ export default {
     }
   }
   .food_types_container{
-    display:flex;
-    flex-wrap: wrap;
     .link_to_food{
       width: 25%;
       padding: 0.3rem 0rem;
+      float:left;
       @include fj(center);
       figure{
         img{
-          margin-bottom: 0.3rem;
           @include wh(1.8rem, 1.8rem);
         }
         figcaption{
